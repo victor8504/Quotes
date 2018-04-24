a8e886e1cac0fb15969b52ce8fcf0d4d1ef3d962
victor njuguna <victormoore254@gmail.com>
2018-04-24 12:21:12 +0300

    create an instance of the class Quotes

--- quotes/src/app/quotes/quotes.component.ts
+++ quotes/src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes = [
+    new Quotes("Victor Njuguna","Speak what you seek until you see what you've said","~ Bobby Shmurda",new Date(2018,2,10)),
+    new Quotes("Michel Antonio","The past is a place of reference, not a place of residence","~ Bruce Springsten",new Date(2018,1,17)),
+    new Quotes("Dj Grooviq","Some people feel the rain others just get wet","~ Bob Marley",new Date(2018,3,23)),
+
+  ]
+  // 
+  // addNewQuote(quote){
+  //   this.quotes.push(quote)
+  // }
+  //
+  // quoteDelete(index){
+  //   if(index){
+  //     this.quotes.splice(index,1)
+  //   }
+  // }
+  //
+  // onClick(index){
+  //   this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  // }
 
   constructor() { }
 
