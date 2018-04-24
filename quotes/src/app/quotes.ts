a6f1d5a3844109a5750e8a415127c19ee62b8218
root <victormoore254@gmail.com>
2018-04-24 15:01:49 +0300

    Auto-generated commit

--- quotes/src/app/quotes.ts
+++
@@ -1,9 +0,0 @@
-export class Quotes {
-  public showDetails:boolean
-  // public upVotes:number
-  // public downVotes:number
-  constructor(public name:string, public quote:string, public author:string, public date:Date){
-    this.showDetails = false;
-
-  }
-}
