556563a5035819fcc6d6da6e6b1c382b348723d8
victor njuguna <victormoore254@gmail.com>
2018-04-24 12:12:26 +0300

    create quotes component

--- quotes/src/app/app.module.ts
+++ quotes/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    QuotesComponent,
   ],
   imports: [
     BrowserModule,
