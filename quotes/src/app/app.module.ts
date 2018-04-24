ccf6c55bdf12ed9615127c9c842d95c1501bd821
victor njuguna <victormoore254@gmail.com>
2018-04-24 12:42:21 +0300

    generate form component

--- quotes/src/app/app.module.ts
+++ quotes/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { DateCountPipe } from './date-count.pipe';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { DateCountPipe } from './date-count.pipe';
     AppComponent,
     QuotesComponent,
     DateCountPipe,
+    QuotesFormComponent,
   ],
   imports: [
     BrowserModule,
